9b4e9bda72b60f38b5ba77ad3481ec1a4cd6d47b
Riganelli Leonardo <leoriganelli1@gmail.com>
2026-02-13 15:13:06 +0100

    sistemazione per telefono e tablet delle varie sezioni

--- src/components/section3/CollaborazioniSection1.jsx
+++ src/components/section3/CollaborazioniSection1.jsx
@@ -1,40 +1,38 @@
-import imgCollaborazioni from '../../img/immagini per section1/DSC_2429.JPG'
-
 const CollaborazioniSection1 = () => {
 
     return(
         <>
-            <section>
-                <h1 className = 'm-3'>Collaborazoni</h1>
+            <section id = "collaborationBigSection">
+                <h1>Collaborazoni</h1>
                 <hr />
 
-                <div className = "m-3">
-                    <h5 className = "m-4">Documenti per diventare soci</h5>
+                <section id = "collaboration">
+                    <div id = "firstDiv">
+                        <h5>Documenti per diventare soci</h5>
 
-                    <p className = "m-4">
-                        Ci siamo quasi presto sarai parte della associazione. Ti basta compilare questi moduli e inviarli alla e-mail. segreteriagenerale@studenticonlestellette.it
-                        Tutti non vedono l’ora di fare la tua conoscenza
-                    </p>      
+                        <p>
+                            Ci siamo quasi presto sarai parte della associazione. Ti basta compilare questi moduli e inviarli alla e-mail. segreteriagenerale@studenticonlestellette.it
+                            Tutti non vedono l’ora di fare la tua conoscenza
+                        </p>      
 
-                    <div className = "m-4">
-                        <a href="">"PDF da mettere"</a>
-                    </div>              
-                </div>
+                        <div>
+                            <a href="">"PDF da mettere"</a>
+                        </div>              
+                    </div>
 
-                <img src={imgCollaborazioni} alt="" className='w-100'/>
+                    <div id = "secondDiv">
+                        <h5>Sei un associazione o ente pubblico/privato?</h5>
 
-                <div className = "m-3">
-                    <h5 className = "m-4">Sei un associazione o ente pubblico/privato?</h5>
+                        <p>
+                            Grazie mille per davi davvero una grossisima mano con questi moduli. Una volta compilati dovrai speidirli a questo indirizzo email segreteriagenerale@studenticonlestellette.it
+                        </p>  
 
-                    <p className = "m-4">
-                        Grazie mille per davi davvero una grossisima mano con questi moduli. Una volta compilati dovrai speidirli a questo indirizzo email segreteriagenerale@studenticonlestellette.it
-                    </p>  
-
-                    <div className = "m-4">
-                        <a href="">"PDF da mettere"</a>
-                        <a href="">"PDF da mettere"</a>
-                    </div>                  
-                </div>
+                        <div>
+                            <a href="">"PDF da mettere"</a>
+                            <a href="">"PDF da mettere"</a>
+                        </div>                  
+                    </div>
+                </section>
             </section>
         </>
     )
--- src/components/section4/VecchiCorsiSection1.jsx
+++ src/components/section4/VecchiCorsiSection1.jsx
@@ -14,152 +14,172 @@ const VecchiCorsiSection1 = () => {
 
             <section>
                 <h1 className = 'm-3'>Vecchi corsi</h1>
-                <hr />
+                    <hr />
 
-                <div className = 'vecchiCorsiSinistra'>
-                    <h5>I corso “Libertà”</h5>
+                    <article className = 'articoleCorsi'>
+                    <div className = 'vecchiCorsiSinistra'>
+                        <h5>I corso “Libertà”</h5>
 
-                    <p>
-                        Il primo Corso "Libertà" si è tenuto nel 2016 presso la Casa Militare Umberto I di Turate ed ha visto la partecipazione di 40 tra ragazzi e ragazze di quarta superiore di alcuni istituti scolastici Lombardi. 
-                        Qui di seguito vediamo riportata la relazione del Corso e una galleria con le foto della settimana. 
-                    </p>
+                        <p>
+                            Il primo Corso "Libertà" si è tenuto nel 2016 presso la Casa Militare Umberto I di Turate ed ha visto la partecipazione di 40 tra ragazzi e ragazze di quarta superiore di alcuni istituti scolastici Lombardi. 
+                            
+                        </p>
 
-                    <div>
-                        <img src={ImgCorso1} alt="" />
+                        <div>
+                            <img src={ImgCorso1} alt="" />
+                        </div>
                     </div>
-                </div>
 
-                <hr />
+                    <hr />
 
-                <div className = 'vecchiCorsiSinistra'>
-                    <h5>II corso “Lealtà”</h5>
+                    <div className = 'vecchiCorsiDestra'>
+                        <h5>II corso “Lealtà”</h5>
 
-                    <p>
-                        Il secondo Corso "Lealtà" si è tenuto nel 2017 sempre presso la casa militare Umberto I di Turate e si è svolto nel mese di giugno. A questa edizione hanno preso parte oltre ai ragazzi lombardi anche una delegazione proveniente da altre regioni italiane, come il Lazio e la Puglia. Questi ragazzi sono rimasti poi molto legati al progetto, così tanto da voler tornare ad aiutare anche per le successive edizioni del Corso.
-                        Qui di seguito allegata la relazione della settimana e una breve galleria fotografica.  
-                    </p>
+                        <p>
+                            Il secondo Corso "Lealtà" si è tenuto nel 2017 sempre presso la casa militare Umberto I di Turate e si è svolto nel mese di giugno. A questa edizione hanno preso parte oltre ai ragazzi lombardi anche una delegazione proveniente da altre regioni italiane, come il Lazio e la Puglia. Questi ragazzi sono rimasti poi molto legati al progetto, così tanto da voler tornare ad aiutare anche per le successive edizioni del Corso.
+                            
+                        </p>
 
-                    <div>
-                        <img src={ImgCorso2} alt="" />
+                        <div>
+                            <img src={ImgCorso2} alt="" />
+                        </div>
                     </div>
-                </div>
 
-                <hr />
+                    <hr />
+                </article>
 
-                <div className = 'vecchiCorsiSinistra'>
-                    <h5>III corso “Tenacia”</h5>
+                <hr className = 'hrCorsiTablet'/>
 
-                    <p>
-                        Il terzo Corso "Tenacia" si è tenuto nel 2017 a distanza di pochi mesi dal secondo ed ancora una volta presso la casa militare Umberto I di Turate. Ha visto i ragazzi impegnati l'ultima settimana di agosto e per la prima volta ha visto anche la partecipazione durante l'intera settimana degli ex-allievi nel ruolo di "Caporali". Alcuni di questi provenivano anche da fuori regione come ad esempio una delegazione di Romani che dopo aver preso parte al secondo Corso hanno deciso di ritornare ad aiutare.
-                        Qui di seguito allegata la relazione della settimana con i commenti di alcuni partecipanti e una breve galleria fotografica.  
-                    </p>
+                <article className = 'articoleCorsi'>
+                    <div className = 'vecchiCorsiSinistra'>
+                        <h5>III corso “Tenacia”</h5>
 
-                    <div>
-                        <img src={ImgCorso3} alt="" />
+                        <p>
+                            Il terzo Corso "Tenacia" si è tenuto nel 2017 a distanza di pochi mesi dal secondo ed ancora una volta presso la casa militare Umberto I di Turate. Ha visto i ragazzi impegnati l'ultima settimana di agosto e per la prima volta ha visto anche la partecipazione durante l'intera settimana degli ex-allievi nel ruolo di "Caporali". Alcuni di questi provenivano anche da fuori regione come ad esempio una delegazione di Romani che dopo aver preso parte al secondo Corso hanno deciso di ritornare ad aiutare.
+                            
+                        </p>
+
+                        <div>
+                            <img src={ImgCorso3} alt="" />
+                        </div>
                     </div>
-                </div>
 
-                <hr />
+                    <hr />
 
-                <div className = 'vecchiCorsiSinistra'>
-                    <h5>IV corso “Fede”</h5>
+                    <div className = 'vecchiCorsiDestra'>
+                        <h5>IV corso “Fede”</h5>
 
-                    <p>
-                        Il quarto Corso "Fede" si è tenuto nel 2018, per la prima volta si è svolto presso la Fondazione Minoprio ed ha visto una partecipazione maggiore rispetto alle scorse edizioni. Ancora una volta sono parecchie le regioni rappresentate, dai nostri allievi.
-                        Qui di seguito allegata la relazione della settimana con i commenti di alcuni partecipanti e una breve galleria fotografica.  
-                    </p>
+                        <p>
+                            Il quarto Corso "Fede" si è tenuto nel 2018, per la prima volta si è svolto presso la Fondazione Minoprio ed ha visto una partecipazione maggiore rispetto alle scorse edizioni. Ancora una volta sono parecchie le regioni rappresentate, dai nostri allievi.
+                            
+                        </p>
 
-                    <div>
-                        <img src={ImgCorso4} alt="" />
+                        <div>
+                            <img src={ImgCorso4} alt="" />
+                        </div>
                     </div>
-                </div>
 
-                <hr />
+                    <hr />
+                </article>
 
-                <div className = 'vecchiCorsiSinistra'>
-                    <h5>V corso “Onore”</h5>
+                <hr className = 'hrCorsiTablet'/>
 
-                    <p>
-                        Il quinto Corso "Onore" si è tenuto nel 2019, per la seconda volta di fila si è svolto presso la Fondazione Minoprio ed ha visto una partecipazione ancora superiore rispetto alle scorse edizioni, con la bellezza di 84 tra ragazzi e ragazze. Ancora una volta sono parecchie le regioni rappresentate dai nostri allievi, ben 11 regioni e per la prima volta ci sono stati 5 ragazzi provenienti dalla sicilia.
-                        Qui di seguito allegata la relazione della settimana con i commenti di alcuni partecipanti e una breve galleria fotografica.  
-                    </p>
+                <article className = 'articoleCorsi'>
+                    <div className = 'vecchiCorsiSinistra'>
+                        <h5>V corso “Onore”</h5>
 
-                    <div>
-                        <img src={ImgCorso5} alt="" />
+                        <p>
+                            Il quinto Corso "Onore" si è tenuto nel 2019, per la seconda volta di fila si è svolto presso la Fondazione Minoprio ed ha visto una partecipazione ancora superiore rispetto alle scorse edizioni, con la bellezza di 84 tra ragazzi e ragazze. Ancora una volta sono parecchie le regioni rappresentate dai nostri allievi, ben 11 regioni e per la prima volta ci sono stati 5 ragazzi provenienti dalla sicilia.
+                            
+                        </p>
+
+                        <div>
+                            <img src={ImgCorso5} alt="" />
+                        </div>
                     </div>
-                </div>
 
-                <hr />
+                    <hr />
 
-                <div className = 'vecchiCorsiSinistra'>
-                    <h5>VI corso “Forza”</h5>
+                    <div className = 'vecchiCorsiDestra'>
+                        <h5>VI corso “Forza”</h5>
 
-                    <p>
-                        Il sesto Corso "Forza" si è tenuto nel 2021, per la prima volta si è svolto presso la Fondazione Nicolò Rusca ad Albate e ha visto una partecipazione di 52 tra ragazzi e ragazze. Anche per questa edizione, seppur con tutte le problematiche legate all'emergenza pandemica in corso, sono state parecchie le regioni rappresentate dai nostri Allievi, ben 9 regioni e per la prima volta sono state rappresentate entrambe le isole maggiori con la partecipazione di una ragazza per ognuna di queste.
-                        Qui di seguito allegata la relazione della settimana con i commenti di alcuni partecipanti e una breve galleria fotografica.  
-                    </p>
+                        <p>
+                            Il sesto Corso "Forza" si è tenuto nel 2021, per la prima volta si è svolto presso la Fondazione Nicolò Rusca ad Albate e ha visto una partecipazione di 52 tra ragazzi e ragazze. Anche per questa edizione, seppur con tutte le problematiche legate all'emergenza pandemica in corso, sono state parecchie le regioni rappresentate dai nostri Allievi, ben 9 regioni e per la prima volta sono state rappresentate entrambe le isole maggiori con la partecipazione di una ragazza per ognuna di queste.
+                            
+                        </p>
 
-                    <div>
-                        <img src={ImgCorso6} alt="" />
+                        <div>
+                            <img src={ImgCorso6} alt="" />
+                        </div>
                     </div>
-                </div>
 
-                <hr />
+                    <hr />
+                </article>
 
-                <div className = 'vecchiCorsiSinistra'>
-                    <h5>VII corso “Coraggio”</h5>
+                <hr className = 'hrCorsiTablet'/>
 
-                    <p>
-                        Il settimo Corso "Coraggio" si è tenuto nel 2022, per la prima volta si è svolto presso l'università e-Campus di Novedrate e ha visto una partecipazione di 70 tra ragazzi e ragazze. Anche per questa edizione, seppur con tutte le problematiche legate all'emergenza pandemica in corso, gli allievi arrivavano da tutto il territorio nazionale, ben 12 regioni sono state rappresentate. 
-                    </p>
+                <article className = 'articoleCorsi'>
+                    <div className = 'vecchiCorsiSinistra'>
+                        <h5>VII corso “Coraggio”</h5>
 
-                    <div>
-                        <img src={ImgCorso7} alt="" />
+                        <p>
+                            Il settimo Corso "Coraggio" si è tenuto nel 2022, per la prima volta si è svolto presso l'università e-Campus di Novedrate e ha visto una partecipazione di 70 tra ragazzi e ragazze. Anche per questa edizione, seppur con tutte le problematiche legate all'emergenza pandemica in corso, gli allievi arrivavano da tutto il territorio nazionale, ben 12 regioni sono state rappresentate. 
+                        </p>
+
+                        <div>
+                            <img src={ImgCorso7} alt="" />
+                        </div>
                     </div>
-                </div>
 
-                <hr />
+                    <hr />
 
-                <div className = 'vecchiCorsiSinistra'>
-                    <h5>VIII corso “Dovere”</h5>
+                    <div className = 'vecchiCorsiDestra'>
+                        <h5>VIII corso “Dovere”</h5>
 
-                    <p>
-                        L'ottavo Corso ''Dovere'' si è tenuto nel 2023, presso l'università e-Campus di Novedrate e ha visto una partecipazione di 40 tra ragazzi e ragazze. Anche per questa edizione gli allievi arrivavano da tutto il territorio nazionale, ben 12 regioni sono state rappresentate.
-                        Qui di seguito allegata la relazione della settimana con i commenti di alcuni partecipanti e una breve galleria fotografica.  
-                    </p>
+                        <p>
+                            L'ottavo Corso ''Dovere'' si è tenuto nel 2023, presso l'università e-Campus di Novedrate e ha visto una partecipazione di 40 tra ragazzi e ragazze. Anche per questa edizione gli allievi arrivavano da tutto il territorio nazionale, ben 12 regioni sono state rappresentate.
+                            
+                        </p>
 
-                    <div>
-                        <img src={ImgCorso8} alt="" />
+                        <div>
+                            <img src={ImgCorso8} alt="" />
+                        </div>
                     </div>
-                </div>
 
-                <hr />
+                    <hr />
+                </article>
 
-                <div className = 'vecchiCorsiSinistra'>
-                    <h5>IX corso “Volonta”</h5>
+                <hr className = 'hrCorsiTablet'/>
 
-                    <p>
-                        da sistemare
-                    </p>
+                <article className = 'articoleCorsi'>
+                    <div className = 'vecchiCorsiSinistra'>
+                        <h5>IX corso “Volonta”</h5>
 
-                    <div>
-                        <img src='' alt="" />
+                        <p>
+                            da sistemare
+                        </p>
+
+                        <div>
+                            <img src='' alt="" />
+                        </div>
                     </div>
-                </div>
 
-                <hr />
+                    <hr />
 
-                <div className = 'vecchiCorsiSinistra'>
-                    <h5>X corso “Audacia”</h5>
+                    <div className = 'vecchiCorsiDestra'>
+                        <h5>X corso “Audacia”</h5>
 
-                    <p>
-                        da sistemare
-                    </p>
+                        <p>
+                            da sistemare
+                        </p>
 
-                    <div>
-                        <img src='' alt="" />
+                        <div>
+                            <img src='' alt="" />
+                        </div>
                     </div>
-                </div>
+                </article>
+
+                <hr className = 'hrCorsiTablet'/>
             </section>
         </>
     )
