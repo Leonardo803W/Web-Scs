1b1c03269259a017bb655eebe69698bbd6f02425
Riganelli Leonardo <leoriganelli1@gmail.com>
2026-05-25 14:24:37 +0200

    improve UX for all the section for all the device

--- src/components/componentsSection/CollaborazioniSection1.jsx
+++ src/components/componentsSection/CollaborazioniSection1.jsx
@@ -7,40 +7,42 @@ const CollaborazioniSection1 = () => {
             <section className = "Collaborazioni">
                 <h1>Collaborazoni</h1>
 
-                    <div className = "documentazioneCollaborazioni">
-                        <h5>Documenti per diventare soci</h5>
+                    <section className = "collaborazioniIntro">
+                        <div className = "documentazioneCollaborazioni">
+                            <h5>Documenti per diventare soci</h5>
 
-                        <p>
-                            Ci siamo quasi presto sarai parte della associazione. Ti basta compilare questi moduli e inviarli alla e-mail. segreteriagenerale@studenticonlestellette.it
-                            Tutti non vedono l’ora di fare la tua conoscenza
-                        </p>      
+                            <p>
+                                Ci siamo quasi presto sarai parte della associazione. Ti basta compilare questi moduli e inviarli alla e-mail. segreteriagenerale@studenticonlestellette.it
+                                Tutti non vedono l’ora di fare la tua conoscenza
+                            </p>      
 
-                        <a 
-                            href="/PDF/01_-_domanda_di_iscrizione_associazione_-_compilabile.pdf"
-                            download
-                            class="btn btn-primary m-3">
-                            PDF modulo per diventare socio
-                        </a>                
-                    </div>
-
-                    <div className = "documentazioneCollaborazioni">
-                        <h5>Sei un associazione o ente pubblico/privato?</h5>
-
-                        <p>
-                            Grazie mille per davi davvero una grossisima mano con questi moduli. Una volta compilati dovrai speidirli a questo indirizzo email segreteriagenerale@studenticonlestellette.it
-                        </p>  
-
-                        <div>
                             <a 
+                                href="/PDF/01_-_domanda_di_iscrizione_associazione_-_compilabile.pdf"
+                                download
                                 class="btn btn-primary m-3">
-                                PDF
-                            </a>   
-                            <a 
-                                class="btn btn-primary m-3">
-                                PDF
-                            </a>   
-                        </div>                  
-                    </div>
+                                PDF modulo per diventare socio
+                            </a>                
+                        </div>
+
+                        <div className = "documentazioneCollaborazioni">
+                            <h5>Sei un associazione o ente pubblico/privato?</h5>
+
+                            <p>
+                                Grazie mille per davi davvero una grossisima mano con questi moduli. Una volta compilati dovrai speidirli a questo indirizzo email segreteriagenerale@studenticonlestellette.it
+                            </p>  
+
+                            <div>
+                                <a 
+                                    class="btn btn-primary m-3">
+                                    PDF
+                                </a>   
+                                <a 
+                                    class="btn btn-primary m-3">
+                                    PDF
+                                </a>   
+                            </div>                  
+                        </div>
+                    </section>
             </section>
         </>
     )
